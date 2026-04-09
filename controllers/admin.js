54451b2e731b1def680ab9ed18f21e4f9d0ca7a6
Yash Kalange <yash.kalange@mindbowser.com>
2026-04-09 20:48:44 +0530

    feat:day17 complete

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
     .then(result => {
@@ -65,7 +72,6 @@ exports.postEditProduct = (req, res, next) => {
   product
     .save()
     .then(result => {
-      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
@@ -87,7 +93,6 @@ exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId)
     .then(() => {
-      console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
     .catch(err => console.log(err));
--- models/product.js
+++ models/product.js
@@ -2,19 +2,19 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
-      // Update the product
       dbOp = db
         .collection('products')
         .updateOne({ _id: this._id }, { $set: this });
