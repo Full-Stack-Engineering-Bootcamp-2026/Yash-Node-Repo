750cee7830b7c5774d2f3eb44d22d468ece21ede
Yash Kalange <yash.kalange@mindbowser.com>
2026-04-10 06:43:39 +0530

    feat:day18 complete

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoConnect  = require("./util/database").mongoConnect
-
+const User = require('./models/user')
 const app = express();
 
 app.set("view engine", "ejs");
@@ -26,6 +26,16 @@ app.use(express.static(path.join(__dirname, "public")));
 //     })
 //     .catch((err) => console.log(er));
 // });
+app.use((req, res, next) => {
+  User.find('5baa2528563f16379fc8a610')
+    .then(user => {
+      console.log('created')
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
