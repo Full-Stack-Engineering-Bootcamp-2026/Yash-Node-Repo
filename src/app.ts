244d33682ef1675d53225bc866eb22ca4aef4c02
Yash Kalange <yash.kalange@mindbowser.com>
2026-04-27 21:41:14 +0530

    feat:day-26 complete

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import express, { Request, Response } from "express";
-// import {multer }from 'multer';
-import path from "path";
-
-import { User } from "./modules/user.js";
-import { Order } from "./modules/order.js";
-import { Product } from "./modules/product.js";
+import path from "path";    
 import mongoose from "mongoose";
-import { log } from "node:console";
-import razorpayRouter from './routes/razor-pay.route.js'
+
 
 const MONGODB_URI = process.env.MONGO_ATLAS_CONNECTION_URI as string;
 
@@ -17,21 +11,19 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/images", express.static("images"));
 
-app.use('/payment',razorpayRouter)
+app.listen(port,()=>{
+  console.log(`server started at ${port}`)
+})
+// const startServer = async () => {
+//   try {
+//     await mongoose.connect(MONGODB_URI);
+//     app.listen(8080);
+//     console.log("MongoDB connected successfully!!!");
+//     console.log("server started at 8080");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-
-
-const startServer = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    app.listen(8080);
-    console.log("MongoDB connected successfully!!!");
-    console.log("server started at 8080");
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-startServer();
+// startServer();
--- src/services/razor-pay.servcie.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import Razorpay from "razorpay";
-
-
-export const razorpayInstance = new Razorpay({
-    key_id:process.env.KEY_ID,
-    key_secret:process.env.KEY_SECRET
-})
